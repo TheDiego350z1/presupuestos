125302cc932df1194d1254b781a112fea715f9f1
Diego García <diego350z1@gmail.com>
2022-05-13 17:18:57 -0600

    Actualización de pantalla de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,23 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div ">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+            </div>
+
+            <div class="py-4 flex justify-center">
+                <a href="{{ route('register') }}" class="a-link">
+                    Registrarte
+                </a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
+
             </div>
         </form>
     </x-jet-authentication-card>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Llamamos a la vista login
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware([
