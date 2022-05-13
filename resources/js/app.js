7cdde9d9f5959eafb623998394154682e9b2468a
Diego García <diego350z1@gmail.com>
2022-05-13 16:50:13 -0600

    Integración de ChartJS y SweetAlert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 
+window.Chart = require('chart.js');
+
+window.Swal = require('sweetalert2');
+
 window.Alpine = Alpine;
 
 Alpine.start();
